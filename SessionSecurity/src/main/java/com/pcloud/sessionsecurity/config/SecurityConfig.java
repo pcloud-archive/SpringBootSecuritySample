f62269bdef3ea62001f513191f2380853c4b4e20
PCloud63514 <pcloud63514@gmail.com>
2021-07-07 20:57:18 +0900

    add

--- SessionSecurity/src/main/java/com/pcloud/sessionsecurity/config/SecurityConfig.java
+++ SessionSecurity/src/main/java/com/pcloud/sessionsecurity/config/SecurityConfig.java
@@ -20,10 +20,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                // 인가할 요청을 설정
                 .authorizeRequests()
+                // antMatchers에 포함된 url에 대해 인증을 요구하지 않음.
                 .antMatchers("/home/**", "/member/**").permitAll()
+                // 나머지 모든 요청에 대해 인증을 요구함.
                 .anyRequest().authenticated()
             .and()
+                // 로그인 form 설정. 별도의 설정을 하지 않으면 기본 로그인 폼을 제공한다.
             .formLogin();
     }
 
---
+++ SessionSecurity/src/main/java/com/pcloud/sessionsecurity/config/security/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,22 @@
+package com.pcloud.sessionsecurity.config.security.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+}
