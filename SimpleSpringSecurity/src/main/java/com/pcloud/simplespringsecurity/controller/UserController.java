6350f927b4dcac0520a0cf6d4625638368202c46
PCloud63514 <pcloud63514@gmail.com>
2021-07-14 23:06:47 +0900

    add

--- SimpleSpringSecurity/src/main/java/com/pcloud/simplespringsecurity/controller/UserController.java
+++ SimpleSpringSecurity/src/main/java/com/pcloud/simplespringsecurity/controller/UserController.java
@@ -3,19 +3,26 @@ package com.pcloud.simplespringsecurity.controller;
 import com.pcloud.simplespringsecurity.dto.UserDto;
 import com.pcloud.simplespringsecurity.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("user")
-@RestController
+@Controller
 public class UserController {
 
     private final UserService userService;
-    @PostMapping("/join")
-    public String join(@RequestBody UserDto userDto) {
-        return "redirect:/home";
+    @PostMapping("/signup")
+    public String signUp(@ModelAttribute UserDto userDto) {
+        log.info("UserDto_NAME:" + userDto.getName());
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/signup")
+    public String signUp() {
+        return "signUp";
     }
 }
